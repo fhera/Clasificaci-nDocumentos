6f26ffe9acfc83690dbcd023d363af6121fd5e82
Fernando <fheraa@gmail.com>
2017-04-30 15:16:31 +0200

    Añadida las frecuencias, aunque no todas

--- Documento.py
+++ Documento.py
@@ -1,9 +1,14 @@
 from io import open
+from collections import Counter
+from tabulate import tabulate
+import math
 import os
 
 
 class Documentos():
     documentos = []
+    num_docs = 0
+
     # Constructor que coge los documentos de la ruta indicada en scandir
     # y guarda los nombres en una lista de documentos.
     def __init__(self):
@@ -13,6 +18,7 @@ class Documentos():
                 if not entry.name.startswith('.') and entry.is_file():
                     lista_documentos.append(entry.name)
         self.documentos = lista_documentos
+        self.num_docs = len(lista_documentos)
 
     # Lee los documentos y guarda en una lista el texto de cada documento
     def leer_documentos(self):
@@ -32,29 +38,56 @@ class Documentos():
         for i in range(len(lista)):
             for documento in lista[i]:
                 palabras = documento.replace("\n", "").split(' ')
-                lista_palabras.append(palabras)
+                lista_palabras.append([i.lower() for i in palabras])
         return lista_palabras
 
     # Frecuencia con la que aparece una palabra en cada documento
     def frecuencia(self):
         docs = self.lista_palabras()
+        frec = []
         for palabras in docs:
-            for palabra in palabras:
-                print("Con la palabra '{}' hay {} coincidencias en el documento {}."
-                      .format(palabra, palabras.count(palabra), docs.index(palabras)))
+            frec.append(Counter(palabras))
+        for i in frec:
+            print("i:", dict(i))
+        return frec
 
-
+    # Nº de veces que aparece una palabra en documentos distintos
     def frecuencia_documental(self):
-        pass
+        docs = self.lista_palabras()
+        frec = {}
+        for i in range(len(docs)):
+            for palabra in docs[i]:
+                # print("Documento {}".format(i), palabra)
+                if palabra in frec.keys() and frec[palabra][0] != i:
+                    frec[palabra][1] += 1
+                else:
+                    frec[palabra] = [i, 1]
+        for palabra in frec.keys():
+            frec[palabra] = frec.get(palabra)[1]
 
+        # print(tabulate(frec))
+        return frec
+
+    # log(N/frec_documental) -> N=nº total de documentos
     def frecuencia_documental_inversa(self):
-        pass
-
+        frec_doc = self.frecuencia_documental()
+        res = {}
+        for palabra in frec_doc:
+            # print("{} = {}".format(palabra,self.num_docs/frec_doc[palabra]))
+            res[palabra] = math.log10(self.num_docs / frec_doc[palabra])
+        return res
+    # Para cada documento tenemos que calcular el peso:
+    # Wi= frecuencia · frec_doc_inversa
     def peso(self):
+        frec = self.frecuencia()
+        print(frec)
         pass
 
 
 doc = Documentos()
-# doc.leer_documentos()
-# doc.lista_palabras()
-doc.frecuencia()
+# print("Leemos los documentos\n", doc.leer_documentos())
+# print("Listamos las palabras de los documentos\n", doc.lista_palabras())
+# print("Frecuencia de palabras en los docs\n", doc.frecuencia())
+# print("Frecuencia docs\n", doc.frecuencia_documental())
+# print("Frecuencia docs inversa\n", doc.frecuencia_documental_inversa())
+doc.peso()
